b319e9949f7b405766fc5a16e8de21e91c761c7e
ladybug010 <devon@agoragames.com>
2011-06-02 16:50:22 -0400

    forcing line return

--- room.java
+++ room.java
@@ -1,6 +1,6 @@
 /*Devon Smith
 Exercize room 3D
-This program will ask the user to input the three dimentions of a room. The inputs will be saved to variables. With those inputs, the program will caclulate the area and perimiter of the room. area, by multiplying the lenght * width * hight and hte perimiter by adding the lenght + width  + height. It will then print out the area and the perimiter to the screen for the user to view.
+This program will ask the user to input the three dimentions of a room. The inputs will be saved to variables. With those inputs, the program will caclulate the area and perimiter of the room. area, by multiplying the length * width * hight and hte perimiter by adding the length + width  + height. It will then print out the area and the perimiter to the screen for the user to view.
 */
 
 import java.util.Scanner;
@@ -10,14 +10,14 @@ public class room
 	public static void main(String[] args)
 	{
 		/* define the variables*/
-		double lenght, width, height, area, perimiter;
+		double length, width, height, area, perimiter;
 		Scanner sc = new Scanner(System.in);
 		
-		/* prompt the user to input room lenght*/
-		System.out.print("Please enter the lenght of the room:  ");
+		/* prompt the user to input room length*/
+		System.out.print("Please enter the length of the room:  ");
 		
 		/*save the input as a variable*/
-		lenght = sc.nextDouble();
+		length = sc.nextDouble();
 		
 		/* prompt the user to input room width*/
 		System.out.print("Please enter the width of the room:   ");
@@ -30,12 +30,12 @@ public class room
 		height = sc.nextDouble();
 		
 		/*using the saved variables, perform calculations*/
-		area = lenght * width * height;
-		perimiter = lenght + width + height;
+		area = length * width * height;
+		perimiter = length + width + height;
 		
 		System.out.printf("The room's area = %.1f\n", area);
 		
-		System.out.printf("The room's perimiter =%.1f\n", perimiter);
+		System.out.printf("The room's perimiter =%.1f\r\n", perimiter);
 		
 	}
 }
