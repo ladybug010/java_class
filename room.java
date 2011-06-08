4d2070fbb68132d346ae93d670b08ceda8e3cfe1
ladybug010 <devon@agoragames.com>
2011-06-08 15:02:53 -0400

    adding additonal calculations

--- room.java
+++ room.java
@@ -1,6 +1,8 @@
 /*Devon Smith
-Exercize room 3D
-This program will ask the user to input the three dimentions of a room. The inputs will be saved to variables. With those inputs, the program will caclulate the area and perimiter of the room. area, by multiplying the length * width * hight and hte perimiter by adding the length + width  + height. It will then print out the area and the perimiter to the screen for the user to view.
+Exercize "room 3D"
+This program will ask the user to input the three dimentions of a room. 
+The inputs will be saved to variables. With those inputs, the program will caclulate the area and perimiter of the room. area, by multiplying the length * width * hight and the perimeter by adding the length + width  + height. It will also calculate the rooms surface area and volume.
+It will then print out the area and the perimeter, surface area, volume-- to the screen for the user to view.
 */
 
 import java.util.Scanner;
@@ -10,7 +12,7 @@ public class room
 	public static void main(String[] args)
 	{
 		/* define the variables*/
-		double length, width, height, area, perimiter;
+		double length, width, height, area, perimeter, surfaceArea, volume;
 		Scanner sc = new Scanner(System.in);
 		
 		/* prompt the user to input room length*/
@@ -30,12 +32,18 @@ public class room
 		height = sc.nextDouble();
 		
 		/*using the saved variables, perform calculations*/
-		area = length * width * height;
-		perimiter = length + width + height;
+		area = length * width;
+		perimeter = length*2 + width*2;
+		surfaceArea = (length * width)*2 +(width * height)*2 + (length * height)*2;
+		volume = length*width*height;
 		
-		System.out.printf("The room's area = %.1f\n", area);
+		System.out.printf("The room's area = %.4f\n", area);
 		
-		System.out.printf("The room's perimiter =%.1f\r\n", perimiter);
+		System.out.printf("The room's perimeter = %.4f\r\n", perimeter);
+		
+		System.out.printf("The room's surface area = %.4f\r\n", surfaceArea);
+		
+		System.out.printf("The room's volume = %.4f\r\n", volume);
 		
 	}
 }
